956046fa891479a540cef798cd9b5dd52f4265e4
Jai Agarwal <jai.agarwal2022@gmail.com>
2022-10-18 17:03:22 +0530

    Creating Navbar and other basic components and adding routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import ErrorBoundary from './Components/ErrorBoundary';
+import About from './Components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <>
+      <ErrorBoundary>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path = "/" element = {<Home/>}/>
+            <Route exact path = "/about" element = {<About/>}/>
+            {/* <Route exact path = "/user" element = {<Users/>}/> */}
+          </Routes>
+        </Router>
+      </ErrorBoundary>
+    </>
   );
 }
 
